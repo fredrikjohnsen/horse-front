f31e70f6b8851591063b20d5b166a7bf5b70df7a
fredrjo <fredrik.johnsen@cebyc.no>
2019-10-10 12:25:56 +0200

    formatting

--- src/Components/raceday.js
+++ src/Components/raceday.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const raceday = (props) => {
+const raceday = ( props ) => {
     return (
-            <tr>
-                <td>{ props.data.place }</td> 
-                <td style={{ textAlign:"left" }}>{ props.data.name }</td>
-                <td style={{ textAlign:"left" }}>{ props.data.skip }</td>
-                <td style={{ textAlign:"right" }}>{ props.data.odds }</td>
-               
-            </tr>
+        <tr>
+            <td>{ props.data.place }</td> 
+            <td style={{ textAlign : "left" }}>{ props.data.name }</td>
+            <td style={{ textAlign : "left" }}>{ props.data.skip }</td>
+            <td style={{ textAlign : "right" }}>{ props.data.odds }</td>
+        </tr>
     )
 }
 export default raceday;
